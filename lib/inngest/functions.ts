b8016a556020e7938fac29ae06e58c02902bc634
Hugo Senzai <hugo@senzai.net>
2026-01-07 22:19:40 +0900

    removed unused import

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -1,7 +1,7 @@
 // lib/inngest/functions.ts
 
 import {inngest} from "@/lib/inngest/client";
-import {NEWS_SUMMARY_EMAIL_PROMPT, PERSONALIZED_WELCOME_EMAIL_PROMPT} from "@/lib/inngest/prompts";
+import { PERSONALIZED_WELCOME_EMAIL_PROMPT } from "@/lib/inngest/prompts";
 import { getAllUsersForNewsEmail } from "@/lib/actions/user.actions";
 import { getWatchlistSymbolsByEmail } from "@/lib/actions/watchlist.actions";
 import { getNews } from "@/lib/actions/finnhub.actions";
